7508b4a1c9798fb09c98b627e44af534d0a08cac
abdirahmanabdullahi <33281290+abdirahmanabdullahi@users.noreply.github.com>
2022-05-15 13:41:14 +0300

    Add files via upload

---
+++ oddAndEvenNumbersUsingWhileLoop/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+
+
+
+fun main(){
+
+    var i = -1
+    while (i < 100){
+        i++
+        if (i % 2 == 0 ){
+            println("The number " + i + " is even.")
+        }else{
+            println("The number " + i + " is odd.")
+        }
+    }
+}
